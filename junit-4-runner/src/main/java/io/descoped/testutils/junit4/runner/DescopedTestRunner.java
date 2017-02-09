63a9c55ec2ad70b9e823bfa3f3c71f857bb200ba
oranheim <oranheim@gmail.com>
2017-02-09 13:43:44 +0100

    initialized JUL logger

--- junit-4-runner/src/main/java/io/descoped/testutils/junit4/runner/DescopedTestRunner.java
+++ junit-4-runner/src/main/java/io/descoped/testutils/junit4/runner/DescopedTestRunner.java
@@ -5,12 +5,15 @@ import io.descoped.container.module.DescopedPrimitive;
 import io.descoped.container.module.factory.DefaultInstanceFactory;
 import io.descoped.container.module.factory.InstanceFactory;
 import io.descoped.container.module.spi.SpiInstanceFactory;
+import io.descoped.logger.logback.handler.LogbackBridgeHandler;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.logging.Level;
 
 /**
  * Created by oranheim on 09/02/2017.
@@ -24,6 +27,7 @@ public class DescopedTestRunner extends BlockJUnit4ClassRunner {
 
     public DescopedTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
+        LogbackBridgeHandler.installJavaUtilLoggerBridgeHandler(Level.INFO);
         factory = DefaultInstanceFactory.get(SpiInstanceFactory.class);
         container = new DescopedContainer<>(factory);
     }
